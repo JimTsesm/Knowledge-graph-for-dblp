7dbbc2021d4e998a4d5bc1ee92a06e8481fd31cf
Dimitris <zas1122@hotmail.com>
2020-04-29 13:37:22 +0300

    add report and resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,19 @@ public class Main {
         Abox abox = new Abox();
         System.out.println("starting");
 
-//        abox.transformPaper();
-//        abox.transformUniversity();
-//        abox.transformAuthor();
-//        abox.link_authors_paper();
-//        abox.transformKeyword();
-//        abox.link_paper_keyword();
-//        abox.transformCitation();
-//        abox.transformReviews();
-//        abox.transformJournal();
-//        abox.transformVolume_linkToJournal();
-//        abox.transformConference();
-//        abox.transformEdition_linkToConference();
-//        abox.link_paper_edition();
+        abox.transformPaper();
+        abox.transformUniversity();
+        abox.transformAuthor();
+        abox.link_authors_paper();
+        abox.transformKeyword();
+        abox.link_paper_keyword();
+        abox.transformCitation();
+        abox.transformReviews();
+        abox.transformJournal();
+        abox.transformVolume_linkToJournal();
+        abox.transformConference();
+        abox.transformEdition_linkToConference();
+        abox.link_paper_edition();
         abox.link_paper_volume();
 
         System.out.println("ending");
